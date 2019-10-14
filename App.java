07acf8e2ba1fa2a867b7e82bbf0b59e7251ded52
Forzr617 <umax2006@outlook.com>
2019-10-14 22:16:22 +0300

    Both command creating coded

--- App.java
+++ App.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 import java.awt.*;
+import java.util.InputMismatchException;;
 
 
 public class App{
@@ -43,16 +44,6 @@ public class App{
             System.out.println( f.getName() );
         }
         System.out.println("Choose the type of command to show\n2.With Net\n2.Without Net\n3.Combine");
-        ystem.out.println("It`s the first start!\nLet`s create your first command!\nType 'Create New' to create new command!");
-        Scanner scanner = new Scanner(System.in);
-
-        if (scanner.nextLine().equals("Create New")) {
-            choosingTypeOfCommand();
-        }
-        else{
-            System.out.println("Try one more time!");
-            showingCommands();
-        }
         Scanner typeToShow = new Scanner(System.in);
         int showingType = typeToShow.nextInt();
         if (showingType == 1) {
@@ -66,13 +57,19 @@ public class App{
 
             File commandToScan = new File("./Commands/WithNet/" + commandName + ".txt");
             startingUrlCommand(commandToScan);
-        } 
+        }
         if (showingType == 2) {
             File withoutNetDirectory = new File("./Commands/WithoutNet");
             for(File f : withoutNetDirectory.listFiles()) {
                 System.out.println(f.getName());
             }
-        } 
+            System.out.println("Type the name of command to start(without '.txt' and without ''''):");
+            Scanner nameOfCommand = new Scanner(System.in);
+            String commandName = nameOfCommand.nextLine();
+
+            File commandToScan = new File("./Commands/WithoutNet/" + commandName + ".txt");
+            startingAppsCommand(commandToScan);
+        }
         if (showingType == 1) {
             File combineDirectory = new File("./Commands/Both");
             for(File f : combineDirectory.listFiles()) {
@@ -87,6 +84,12 @@ public class App{
 
         if (typeOfCommand == 1) {
             creatingWithNetCommand();
+        }
+        if (typeOfCommand == 2) {
+            creatingWithoutNetCommand();
+        }
+        if (typeOfCommand == 3) {
+            creatingCombineCommands();
         }
     }
     public static void creatingWithNetCommand() {
@@ -119,13 +122,13 @@ public class App{
         }
         catch(IOException er) {
             System.out.println("Error");
-        }        
+        }
     }
-    public static void startingUrlCommand(File withCommand) {
+    public static void startingUrlCommand(File withURL) {
         try{
             try{
                 try{
-                    Scanner fileScanner = new Scanner(withCommand);
+                    Scanner fileScanner = new Scanner(withURL);
                     while(fileScanner.hasNextLine()) {
                         Desktop device = Desktop.getDesktop();
                         URI forBrowsing = new URI(fileScanner.nextLine());
@@ -142,6 +145,114 @@ public class App{
         }
         catch(IOException e) {
             System.out.println("Error");
+        }
+    }
+    public static void creatingWithoutNetCommand() {
+        try{
+            System.out.println("Creating command with Internet!\nChoose how many apps or files will be in your command");
+            Scanner howManyURL = new Scanner(System.in);
+            int howManyUr = howManyURL.nextInt();
+            System.out.println("Enter the command name: ");
+            Scanner commandName = new Scanner(System.in);
+            File command = new File("./Commands/WithoutNet/" + commandName.nextLine() + ".txt");
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter(command));
+            for(int i = 0; i != howManyUr; i++){
+                try{
+                    System.out.println("Enter the path to file or app: ");
+                    Scanner pathScanner = new Scanner(System.in);
+                    String path = pathScanner.nextLine();
+                    //Adding url`s to file
+                    fileWriter.write(path);
+                    fileWriter.newLine();
+                }
+                catch(InputMismatchException er) {
+                    System.out.println("Error");
+                }
+            }
+            fileWriter.close();
+            System.out.println("Would you like to test this command?\ny/n");
+            Scanner test = new Scanner(System.in);
+            String willStart = test.nextLine();
+            if (willStart.equals("y")) {
+                startingAppsCommand(command);
+            }
+            if (willStart.equals("n")) {
+                showingCommands();
+            }
+        }
+        catch(IOException er) {
+            System.out.println("Error");
+        }
+    }
+    public static void startingAppsCommand(File withApps) {
+        try{
+            try{
+                try{
+                    Scanner fileScanner = new Scanner(withApps);
+                    while(fileScanner.hasNextLine()) {
+                        Desktop device = Desktop.getDesktop();
+                        File app = new File(fileScanner.nextLine());
+                        device.open(app);
+                    }
+                }
+                catch(FileNotFoundException error) {
+                    System.out.println("Error");
+                }
+            }
+            catch(IllegalArgumentException er) {
+                System.out.println("Error");
+            }
+        }
+        catch(IOException e) {
+            System.out.println("Error");
+        }
+    }
+    public static void creatingCombineCommands() {
+      try{
+        try{
+            System.out.println("Type the name of your combine command: ");
+            Scanner nameOfCommand = new Scanner(System.in);
+            String commandName = nameOfCommand.nextLine();
+
+            //Part with url`s
+            System.out.println("How many url`s will be in your combine command?");
+            Scanner howManySites = new Scanner(System.in);
+            int sitesCount = howManySites.nextInt();
+
+            //Part with apps/files
+            System.out.println("How many apps and files will be in your combine command?");
+            Scanner howManyApps = new Scanner(System.in);
+            int appsCount = howManyApps.nextInt();
+
+
+            //Writing into the .txt file
+            File command = new File("./Commands/Both/" + commandName + ".txt");
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter(command));
+
+            for(int i = 0; i != sitesCount; i++) {
+                System.out.println("Enter the url: ");
+                Scanner urlTaker = new Scanner(System.in);
+                String url = urlTaker.nextLine();
+
+                fileWriter.write(url);
+                fileWriter.newLine();
+            }
+            for (int f = 0; f != appsCount; f++) {
+              System.out.println("Enter the path to the app/file: ");
+              Scanner pathTaker = new Scanner(System.in);
+              String path = pathTaker.nextLine();
+
+              fileWriter.write(path);
+              fileWriter.newLine();
+            }
+            fileWriter.close();
+          }
+          catch(InputMismatchException er) {
+            System.out.println("Input Mismatch Error");
+          }
+        }
+        catch(IOException e) {
+          System.out.println("IO Error");
         }
     }
 }
